aaedd827d1afda37f556f2c294ea305872c005f9
p000078 <p000078@cpe-172-100-100-18.twcny.res.rr.com>
2023-06-07 13:27:40 +0900

    first commit

---
+++ js/swiper.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    autoplay: true,
+    effect: 'fade',
+    speed: 1000,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
